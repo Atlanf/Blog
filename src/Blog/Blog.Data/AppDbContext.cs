7af49e31143d5b2a82f0f839e8ce14650522b74f
Roman Yarmolovich <Smile182382@gmail.com>
2020-11-25 11:51:30 +0300

    Configured project structure. Added DataExtensions class.

---
+++ src/Blog/Blog.Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blog.Data
+{
+    public class AppDbContext: IdentityDbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+            this.ChangeTracker.LazyLoadingEnabled = false;
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ src/Blog/Blog.Data/DataExtensions.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Data
+{
+    public static class DataExtensions
+    {
+        public static IServiceCollection AddDataServices(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddDbConnection(config);
+            services.AddRepositories();
+
+            return services;
+        }
+
+        private static IServiceCollection AddRepositories(this IServiceCollection services)
+        {
+            return services;
+        }
+
+        private static IServiceCollection AddDbConnection(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddDbContext<AppDbContext>(options => {
+                options.UseSqlServer(
+                    config.GetConnectionString("BlogDbConnection"),
+                    builder =>
+                    {
+                        builder.MigrationsAssembly("Blog.Data");
+                    });
+            });
+            return services;
+        }
+    }
+}
